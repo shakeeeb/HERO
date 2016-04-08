6c3d267f7c39f7877b65e7694af26a0a4ce427ce
Terrell Mack <terrell.mack@stonybrook.edu>
2016-04-08 13:18:01 -0400

    Added file structure template Created outline for drawing workspace

--- src/main/webapp/resources/scripts/widgets/drawing.js
+++ src/main/webapp/resources/scripts/widgets/drawing.js
@@ -1,3 +1,10 @@
 /**
  * Created by Terrell Mack on 3/30/16.
+ * Drawing application for HERO
  */
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
