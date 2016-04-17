f29667cecf18923a545274f17c786b18b5bae856
Terrell Mack <terrell.mack@stonybrook.edu>
2016-04-17 18:39:37 -0400

    Pages load into workspace

--- src/main/java/controller/ChapterWorkspaceController.java
+++ src/main/java/controller/ChapterWorkspaceController.java
@@ -4,6 +4,7 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import data.DbContext;
 import data.model.Chapter;
+import data.model.Page;
 import data.model.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,9 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * Created by mk on 3/31/16.
@@ -30,46 +35,104 @@ public class ChapterWorkspaceController {
         return "chapter-workspace";
     }
 
-    @RequestMapping(value="workspace/chapter/{chapterID}", method = RequestMethod.GET)
-    public @ResponseBody String getChapter(@PathVariable(value="chapterID") String chapterID) {
-
+    @RequestMapping(value="workspace/load/{chapterID}", method = RequestMethod.GET)
+    public @ResponseBody JsonObject loadChapter(@PathVariable(value="chapterID") String chapterID) {
+        JsonObject json = new JsonObject();
         System.out.println("Looking for: " + chapterID);
 
         // check if user is logged in
         UserService userService = UserServiceFactory.getUserService();
         if(userService.isUserLoggedIn() == false) {
-            return "Error: User not logged in";
+            return json;//"Error: User not logged in";
+        }
+
+        // load user from the datastore
+        UserData user = db.userRepo.getUserById(userService.getCurrentUser().getEmail());
+        if(user == null) {
+            return json;//"Error: Unable to load user from datastore";
+        }
+
+        // load the chapter
+        Chapter chapterToLoad = db.chapterRepo.getById(chapterID);
+        if(chapterToLoad == null) {
+            return json;//"Error: Unable to load chapter from datastore";
         }
 
         // check if user owns chapter being requested
+        if(chapterToLoad.getAuthor().getEmail() != user.getEmail()) {
+            return json;//"Error: User does not have access to chapter";
+        }
+
+        // TODO check if the chapter being requested is editable (still in draft mode)
+//        if(chapterToLoad.isPublished() == false) {
+//            return "Error: Chapter is no longer editable";
+//        }
+
+        // load chapter as a String/JSon
+        Gson gson = new GsonBuilder().create();
+        json.add("Chapter", gson.toJsonTree(chapterToLoad));
+
+        Page rootPage = chapterToLoad.getRoot();
+        if(rootPage == null) {
+            return json; // Unable to load root
+        }
+
+        ArrayList<Page> allPages = new ArrayList<Page>();
+        rootPage.getAllPages(allPages);
+        if (allPages == null) {
+            return json; // unable to load pages
+        }
+
+        json.add("Pages", gson.toJsonTree(allPages));
+        System.out.println("Chapter, Pages: " + json.toString());
+
+        return json;
+
+    }
+
+    // load the pages for a chapter
+    @RequestMapping(value="workspace/load/pages/{chapterID}", method = RequestMethod.GET)
+    public @ResponseBody JsonObject loadChapterPages(@PathVariable(value="chapterID") String chapterID) {
+        JsonObject json = new JsonObject();
+
+        System.out.println("Loading pages for: " + chapterID);
+        // check if user is logged in
+        UserService userService = UserServiceFactory.getUserService();
+        if(userService.isUserLoggedIn() == false) {
+            return json;//"Error: User not logged in";
+        }
+
+        // load user from the datastore
         UserData user = db.userRepo.getUserById(userService.getCurrentUser().getEmail());
         if(user == null) {
-            return "Error: Unable to load user from datastore";
+            return json;//"Error: Unable to load user from datastore";
         }
-
-        // check if the chapter being requested is editable (still in draft mode)
 
         // load the chapter
-//        Chapter chapterToLoad = db.chapterRepo.getById(chapterID);
-//        if(chapterToLoad == null) {
-//            return "Error: Unable to load chapter from datastore";
-//        }
-        //System.out.println(chapterToLoad.getName());
-
-        List<Chapter> chapters = db.chapterRepo.getAll();
-        if(chapters == null) {
-            System.out.println("Unable to load any chapters from the datastore");
-        } else {
-            System.out.println("Number of chapters in datastore: " + chapters.size());
+        Chapter chapterToLoad = db.chapterRepo.getById(chapterID);
+        if(chapterToLoad == null) {
+            return json;//"Error: Unable to load chapter from datastore";
         }
-        //System.out.println(chapters.size());
-//        if(chapterToLoad != null) {
-//            System.out.println(chapterToLoad.getName());
-//        } else {
-//            System.out.println("Unable to load Chapter: " + chapterID);
-//        }
 
-        // return the chapter as a String/JSON
-        return chapterID;
+        // check if user owns chapter being requested
+        if(chapterToLoad.getAuthor().getEmail() != user.getEmail()) {
+            return json;//"Error: User does not have access to chapter";
+        }
+
+        Page rootPage = chapterToLoad.getRoot();
+        if(rootPage == null) {
+            return json; // Unable to load root
+        }
+
+        ArrayList<Page> allPages = new ArrayList<Page>();
+        rootPage.getAllPages(allPages);
+        if (allPages == null) {
+            return json; // unable to load pages
+        }
+
+        // load up the pages into a json
+        Gson gson = new GsonBuilder().create();
+        json.add("Pages", gson.toJsonTree(allPages));
+        return json;
     }
 }
