3425bd80e06e2254bdf6843b2acfefce12e30a6d
Benjamin Strumeyer <benjamin.strumeyer@stonybrook.edu>
2016-04-18 18:50:11 -0400

    Edited Reader Page

--- src/main/java/controller/ReaderPageController.java
+++ src/main/java/controller/ReaderPageController.java
@@ -1,48 +1,128 @@
 package controller;
 
-        import com.google.appengine.api.users.User;
-        import com.google.appengine.api.users.UserService;
-        import com.google.appengine.api.users.UserServiceFactory;
-        import data.DbContext;
-        import data.model.Chapter;
-        import data.model.Page;
-        import data.model.Series;
-        import data.model.UserData;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.ModelMap;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
+import com.google.appengine.repackaged.com.google.gson.JsonArray;
+import com.googlecode.objectify.cmd.Query;
+import data.DbContext;
+import data.model.Chapter;
+import data.model.Page;
+import data.model.Series;
+import data.model.UserData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mk on 3/31/16.
  */
 
 @Controller
+//@RequestMapping("read")
 public class ReaderPageController {
-    @RequestMapping(value="read", method = RequestMethod.GET)
-    public String readerPageController(ModelMap model) {
-        System.out.println("Going to reader-page ");
 
-        DbContext db = new DbContext();
+    protected DbContext db = new DbContext();
+//
+//    @RequestMapping(value="search/refine/{query}", method = RequestMethod.GET)
+//    public @ResponseBody String searchResultController(@PathVariable(value="query") String query, ModelMap model, HttpSession session, HttpServletRequest req) {
+//        System.out.println("refining the search");
+
+    @RequestMapping(value = "read/{chapter-ID}", method = RequestMethod.GET)
+    public String readController(@PathVariable(value = "chapter-ID") String chapterID, ModelMap model, HttpSession session, HttpServletRequest req) {
+        System.out.println("Going to reader-page");
+//        // get the chapter ID
+//
+//        System.out.println("Chapter ID: " + chapterID);
+//
+//        // load the chapter using the chapter ID
+//
+//        // load the model with the chapter ID
+//        model.addAttribute("chapterID", chapterID);
+
+//        String currentComic = req.getParameter("read-form");//.toLowerCase(); // CHANGE SEARCH INPUT
+//        System.out.println(currentComic);
+
+        // grabbing the series we need to read from url
+//        System.out.println("Series Requested: " + name);
+
+        // grab the actual series from the DB
+        return "read";
+    }
+
+    @RequestMapping(value = "/read/get/{chapter-ID}", method = RequestMethod.GET)
+    public String getChapter(@PathVariable(value = "chapter-ID") String chapterID, ModelMap model, HttpSession session, HttpServletRequest req) {
+        System.out.println("fetching chapter...");
+        System.out.println("The Chapter ID from the URL is: " + chapterID);
+        Chapter c = db.chapterRepo.getById(chapterID);
+        System.out.println("grabbed chapter ID: " + c.getChapterId());
+
+        Page currentPage = db.seriesRepo.getById("One_Piece").getChapters().get(0).getRoot();
+        System.out.println(currentPage.toString());
+
+        System.out.println(" The chapter ID of this chapter object is " + db.seriesRepo.getById("One_Piece").getChapters().get(0).getChapterId());
+
+        System.out.println("The image associated with this chapter object is: " + db.seriesRepo.getById("One_Piece").getChapters().get(0).getRoot().getImagePath());
+
+        model.addAttribute("imagePath", db.seriesRepo.getById("One_Piece").getChapters().get(0).getRoot().getImagePath());
+        model.addAttribute("chapterID", chapterID);
+
+//        System.out.println("The object chapter ID is: " +db.chapterRepo.getById(chapterID).toString());
+//        System.out.println("The root Page is: " + db.chapterRepo.getById(chapterID).getRoot().toString());
+//        System.out.println("The next Page is: " + db.chapterRepo.getById(chapterID).getRoot().getNext().toString());
+//
+//        System.out.println(db.seriesRepo.getById("One_Piece").getChapters().get(0).getRoot().getPageId());
+//        System.out.println("The Chapter number is: " + currentChapter.getChapterNumber());
+//        System.out.println(db.seriesRepo.getById("One_Piece").getChapters().get(0).toString());
+
+//        System.out.println(currentChapter.getChapterId());
+//        Series chap = null;
+//        Query<Series> q = null;
+//        if(chapterID == null){
+//            // new search
+//            System.out.println("Search was null");
+//            //q = newSearch(searchInput);
+//        } else {
+//            // refine a prior search
+//            // grab the query... if it's null, just grab everything
+//            // okay so i can't store queries in the session object
+//            // ughhhhhhhh
+//            //q = (Query<Series>)session.getAttribute("priorQuery");
+//            System.out.println("aahhh "+ db.seriesRepo.getById("One_Piece").getChapters().size());
+//
+//            //chap = db.chapterRepo.getById("the secret weddingCatbug");
+//            //q = refineSearch(q, genre, tag, author, rating, date);
+//
+//        }
+//        System.out.println("query returned: "+ chap.toString() );
+//        List<> s = q.list();
+//        for(Series s3:s){
+//            System.out.println(s3.toString());
+//        }
+//        System.out.println("Test" + s.toString());
+//        // return things as JSON
+//        model.addAttribute("seriesList", s.toString());
 
 
-        UserService service = UserServiceFactory.getUserService();
-        User u = service.getCurrentUser();
-        db.userRepo.create(u.getNickname());
-        UserData Oda = db.userRepo.getUserById(u.getNickname());
+        // transform the series into a JSON
 
-//        Series newSeries = db.seriesRepo.create("One Piece", "Fiction", Oda, "Best MangaEver");
-//        Chapter newChapter = db.chapterRepo.create("Luffy meets Boa", Oda, newSeries, 1);
-//        Page pageOne = db.pageRepo.create(newSeries, newChapter, 1);
-//        Page pageTwo = db.pageRepo.create(newSeries, newChapter, 2);
-//        Page pageThree = db.pageRepo.create(newSeries, newChapter, 3);
-//        Page wowWhataShortChapter = db.pageRepo.create(newSeries, newChapter, 4);
+        Gson gson = new GsonBuilder().create();
+//       gson.toJson(newChapter, System.out);
 
-
+        // return the JSON string to wherever calls this controller
+//        String jsonString = gson.toJson(currentChapter);
+//        System.out.println(jsonString);
+        //return jsonString; //the json String
         return "read";
     }
 }
-// QUERY FOR THE PAGE
-// Load a model with the page info.
-// Go to view.
-// Use the information that was used for the page, which should be the page URL.
