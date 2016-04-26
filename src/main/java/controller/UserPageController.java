d9b3cdfc5e5d69bc47b3ea1167e236fab99e8e2e
MK Yip <miuki.yip@stonybrook.edu>
2016-04-25 22:00:40 -0400

    User page displays series of the person!

--- src/main/java/controller/UserPageController.java
+++ src/main/java/controller/UserPageController.java
@@ -1,9 +1,18 @@
 package controller;
 
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
+import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import data.DbContext;
+import data.model.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by mk on 3/31/16.
@@ -11,9 +20,59 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserPageController {
+
+    protected DbContext db = new DbContext();
     @RequestMapping(value="user", method = RequestMethod.GET)
     public String userPageController(ModelMap model) {
         System.out.println("Going to user-page ");
         return "user";
     }
+
+    @RequestMapping(value = "/user/test/", method = RequestMethod.GET)
+    public String getUserLookup(@RequestParam(value = "userID") String userID, ModelMap model, HttpSession session, HttpServletRequest req) {
+        //http://localhost:8080/user/test/?userID=test.test.test.test
+
+        System.out.println("User:" +  userID);
+        return "user";
+    }
+
+    @RequestMapping(value="user/lookup/{userEmail}", method = RequestMethod.GET)
+    public String loadUser(@PathVariable(value="userEmail") String userEmail, ModelMap model) {
+        System.out.println("Going to user-page");
+        return "user";
+    }
+
+    @RequestMapping(value="user/get/", method = RequestMethod.GET)
+    public @ResponseBody
+    JsonObject getUser(@RequestParam(value="userToLookUpEmail") String userToLookUpEmail, ModelMap model, HttpSession session, HttpServletRequest req, HttpServletResponse res) {
+        System.out.println("Got here");
+        JsonObject json = new JsonObject();
+        Gson gson = new GsonBuilder().create();
+
+        System.out.println(userToLookUpEmail);
+
+        // load user from the datastore
+        UserData user = db.userRepo.getUserById(userToLookUpEmail);
+        System.out.println(user);
+
+        String nickname = user.getNickname();
+        String about_me = user.getAboutMe();
+        String userEmail = user.getEmail();
+        ArrayList series_list = new ArrayList();
+
+        System.out.println("userEmail: " + userEmail);
+
+        json.add("nickname", gson.toJsonTree(nickname));
+        json.add("about_me", gson.toJsonTree(about_me));
+        series_list = user.getMySeries();
+        json.add("series_list", gson.toJsonTree(series_list));
+
+        System.out.println(json);
+
+        //Load this users settings
+
+        return json;
+    }
+
+
 }
---
+++ src/main/webapp/resources/scripts/jquery/user-handler.js
@@ -0,0 +1,65 @@
+/**
+ * Created by mk on 4/21/16.
+ */
+
+$(document).ready(function() {
+    var currentUserEmail = null;
+    var about_me;
+    var nickname;
+    var series_list;
+    currentUserEmail = $("#userEmail").html();
+    console.log("Loading the page!");
+    //loadPage(currentUserEmail);
+    //http://localhost:8080/user/
+    var userToLookUpEmailHardCoded = "Jamez@citi.sandwich.sleep.nap";//encodeURI("terrell.mack@stonybrook.edu");
+
+    loadPage(userToLookUpEmailHardCoded);
+    function loadPage(userToLookUpEmail) {
+        $.getJSON("/user/get/",{"userToLookUpEmail" : userToLookUpEmail}, function(data) {
+        }).done(function(data) {
+
+                console.log(data);
+                nickname = data.members.nickname.value;
+                about_me = data.members.about_me.value;
+
+                //for loop to get all the user series into the arraylist
+                length_of_array = data.members.series_list.elements.length;
+
+                for (i = 0; i <length_of_array; i++) {
+                    series_list = data.members.series_list.elements[i];
+                }
+
+                console.log("CAT ARE COOL.");
+                console.log(series_list);
+
+
+                $("#user-information").html(nickname +"<br><br><br>"+ about_me);
+
+                console.log(data.members.series_list.elements[0].members.name.value);
+                console.log(data.members.series_list.elements[0].members.description.value);
+
+                //create the rest of the series to show in the div with the list of series
+                for (var l = 0; l < length_of_array; l++)
+                {
+                    var seriesHTML = "<span class=\"orange-span one-story\" id=\"authored-story-"+i +"\">" +
+                        "<span class=\"author-story-wrap\" id=\"authored-story-"+i +"-image\">" +
+                        "<img src=\"IMAGEHERE.png\" height=\"100px\" width=\"100px\" > </span>" +
+                        "<span class=\"author-story-wrap\" id=\"authored-story-"+i+"-information\">"    +
+                        "<span class=\"orange-span user-story-title\" id=\"authored-story-"+i+"-title\">"    +
+                        data.members.series_list.elements[l].members.name.value   +
+                        "</span>"  +
+                        "<span class=\"orange-span user-story-tags\" id=\"authored-story-"+i+"-tags\" >"   +
+                        data.members.series_list.elements[l].members.description.value   +
+                        "</span></span></span><br>";
+                    $("#all_the_series").append(seriesHTML);
+                }
+
+            })
+            .fail(function() {
+                console.log(userToLookUpEmail);
+                console.log("Could not get data");
+            });
+    }
+
+});
+
